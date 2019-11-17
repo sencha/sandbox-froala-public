f30476ed78b4e49fa52798a222a6e43434f510ae
Brandon Donnelson <branflake2267@gmail.com>
2019-11-17 07:36:03 -0800

    adjust example

--- simple-webpack-project/index.js
+++ simple-webpack-project/index.js
@@ -6,10 +6,9 @@ import FroalaEditor from 'froala-editor';
 // 2. Import a Froala Editor plugin(s)
 import 'froala-editor/js/plugins/align.min.js';  
 
-// 3. Import the css
+// 3. Import the Froala Editor css
 // The webpack style-loader & css-loader are required to import css here. 
 import 'froala-editor/css/froala_editor.pkgd.min.css';
-import 'froala-editor/css/froala_style.min.css';
 
 // 4. Render the Froala editor in the <div/> with id of `editor`.
 new FroalaEditor('#editor');
